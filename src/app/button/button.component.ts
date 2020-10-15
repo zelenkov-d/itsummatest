26da8a165e47098172911f5fe90ab5baecc7a5f4
Dmitriy Zelenkov <zelen.draks@mail.ru>
2020-10-16 00:21:16 +0800

    Updated button component

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { faStar, IconDefinition, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { faBan, faSignInAlt, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-button',
@@ -6,14 +8,37 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
+  
+  @Input() iconType: string;
+  @Input() text = '';
 
-  @Input() icon: string;
-  @Input() text: string;
-  @Input() action: any;
+  @Output() clicked = new EventEmitter();
+
+  icon: IconDefinition;
+  isText = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.iconType === 'fav') {
+      this.icon = faStar;
+    } else if (this.iconType === 'ban') {
+      this.icon = faBan;
+    } else if (this.iconType === 'arrow') {
+      this.icon = faSignInAlt;
+    } else if (this.iconType === 'del') {
+      this.icon = faTrashAlt;
+    } else if (this.iconType === 'exit') {
+      this.icon = faTimesCircle;
+    }
+
+    if (this.text) {
+      this.isText = true;
+    }
+  }
+
+  action(): void {
+    this.clicked.emit();
   }
 
 }
