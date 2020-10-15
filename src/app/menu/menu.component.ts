0dcf24bd09886da53caa965bc8a912068964066f
Dmitriy Zelenkov <zelen.draks@mail.ru>
2020-10-16 00:20:20 +0800

    Updated maenu component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  @Output() clickedFavList = new EventEmitter();
+  @Output() clickedBanList = new EventEmitter();
+
+  public favIcon = 'fav';
+  public favText = 'Избранное';
+
+  public banIcon = 'ban';
+  public banText = 'Черный список';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  clickFavBtn(): void {
+    this.clickedFavList.emit();
+  }
+
+  clickBanBtn(): void {
+    this.clickedBanList.emit();
+  }
 }
